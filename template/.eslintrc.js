26b71bce6d2476a07f225538bd09643a68c31fed
乔般 <dengqiaobo48497@tanzhou.cn>
2022-02-28 14:00:05 +0800

    demo

--- template/.eslintrc.js
+++ template/.eslintrc.js
@@ -19,10 +19,14 @@ module.exports = {
   ],
   // add your custom rules here
   'rules': {
+    {{#if_eq lintConfig "standard"}}
+    "camelcase":0,
     // allow paren-less arrow functions
     'arrow-parens': 0,
+    "space-before-function-paren": 0,
     // allow async-await
     'generator-star-spacing': 0,
+    {{/if_eq}}
     // allow debugger during development
     'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0
   },
